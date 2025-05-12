4b1397fc2b419610289b8a3d73300a9c8feeebe6
Lucas Espitia <espitia.174114@e.unavarra.es>
2025-05-12 21:04:00 +0200

    Arreglando las rutas para el deploy

--- src/constants/rutas.jsx
+++ src/constants/rutas.jsx
@@ -1,10 +1,10 @@
 const rutas = {
-  home: "/wiki-rick-and-morty/",
-  personajes: "/wiki-rick-and-morty/Personajes",
-  ubicaciones: "/wiki-rick-and-morty/Ubicaciones",
-  episodios: "/wiki-rick-and-morty/Episodios",
-  nosotros: "/wiki-rick-and-morty/AcercaDeNosotros",
-  faq: "/wiki-rick-and-morty/FaQ",
+  home: "/",
+  personajes: "/Personajes",
+  ubicaciones: "/Ubicaciones",
+  episodios: "/Episodios",
+  nosotros: "/AcercaDeNosotros",
+  faq: "/FaQ",
 };
 
 export default rutas;
